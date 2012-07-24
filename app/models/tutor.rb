fc6e903204b164b36b028761e7b884c2dd85fe62
Bin Zhu <binzhu1070@gmail.com>
2012-07-24 11:01:25 -0400

    server static assets update

--- app/models/tutor.rb
+++ app/models/tutor.rb
@@ -1,7 +1,11 @@
 class Tutor < ActiveRecord::Base
   attr_accessible :rate, :resume, :transcript, :univ_identifier, :university, :user_id, :approved
-  has_attached_file :resume
-  has_attached_file :transcript
+  has_attached_file :resume,
+                    :url  => "/assets/resume/:id/:basename.:extension",
+                    :path => ":rails_root/public/assets/resume/:id/:basename.:extension"
+  has_attached_file :transcript,
+                    :url  => "/assets/resume/:id/:basename.:extension",
+                    :path => ":rails_root/public/assets/resume/:id/:basename.:extension"  
   belongs_to :user
   has_many :meetings
   has_many :subjects_tutors
