afc938c2201deee31c8905b3d8956976bdb64cf7
Bin Zhu <binzhu1070@gmail.com>
2012-08-05 00:14:35 -0400

    ipn crsf disabled

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery
+  protect_from_forgery :except => :ipn_notification 
   
   before_filter :authticate, :except => [:login]
   
