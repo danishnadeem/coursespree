2f493da4b6558d744ea61695799d134972fc339c
mahheki <mahhek.khan@gmail.com>
2013-02-27 12:50:39 +0500

    updated

--- db/migrate/20130218073059_add_start_and_end_meeting_email_sent_to_meeting.rb
+++ db/migrate/20130218073059_add_start_and_end_meeting_email_sent_to_meeting.rb
@@ -1,10 +1,8 @@
 class AddStartAndEndMeetingEmailSentToMeeting < ActiveRecord::Migration
   def change
-#    add_column :meetings, :start_meeting_email_sent, :boolean, :default => false
-#    add_column :meetings, :end_meeting_email_sent, :boolean, :default => false
-#    add_column :meetings, :upcoming_meeting_email_six_hours_before, :boolean, :default => false
-#    add_column :meetings, :upcoming_meeting_email_twelve_hours_before, :boolean, :default => false
-#    remove_column :meetings, :start_meeting_email_sent, :integer, :default => 0
-    remove_column :meetings, :end_meeting_email_sent, :integer, :default => 0
+    add_column :meetings, :start_meeting_email_sent, :boolean, :default => false
+    add_column :meetings, :end_meeting_email_sent, :boolean, :default => false
+    add_column :meetings, :upcoming_meeting_email_six_hours_before, :boolean, :default => false
+    add_column :meetings, :upcoming_meeting_email_twelve_hours_before, :boolean, :default => false
   end
 end
