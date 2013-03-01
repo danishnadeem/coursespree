6b0a45185b4caef949ce0707a752dbe9b5a9fa6f
mahheki <mahhek.khan@gmail.com>
2013-03-01 15:22:18 +0500

    updated

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,15 +3,16 @@ class UserMailer < ActionMailer::Base
   
   def student_request_for_meeting_to_tutor(meeting_id)
     @meeting = Meeting.find_by_id(meeting_id)
-    meeting_tutor = @meeting.tutor
     @tutor = @meeting.tutor.user
     @student = @meeting.user
-    #        mail(:to => meeting_tutor.user.email, :subject => "Student requests you for meeting")
 
     mail(:to => "mahhek.khan@gmail.com", :subject => "Student requests you for meeting, so please approve the pending meeting") do |format|
       format.text
       format.html { render "email_sent_successfully" }
     end
+
+    #    mail(:to => @tutor.email, :cc => @student.email, :subject => "Student requests you for meeting, so please approve the pending meeting")
+
   end
 
   def tutor_accept_meeting_request(meeting_id)
@@ -21,28 +22,22 @@ class UserMailer < ActionMailer::Base
     all_subadmins = Subadmin.all
     all_superadmins = Superadmin.all
 
-    #    mail(:to => @student.email, :subject => "Tutor accept your meeting request")
-    mail(:to => "mahhek.khan@gmail.com", :subject => "Tutor accept your meeting request") do |format|
-      format.text
-      format.html { render "email_sent_successfully" }
-    end
+    mailling_list = []
 
     all_subadmins.each do |subadmin|
       if subadmin.user.department == @student.department
-        #      mail(:to => subadmin.user.email, :subject => "Tutor accept the stutent's meeting request")
-        mail(:to => "mahhek.khan@gmail.com", :subject => "Tutor accept the stutent's meeting request") do |format|
-          format.text
-          format.html { render "email_sent_successfully" }
-        end
+        mailling_list << subadmin.user.email
       end
     end
+
     all_superadmins.each do |superadmin|
-      #      mail(:to => superadmin.user.email, :subject => "Tutor accept the stutent's meeting request")
-      mail(:to => "mahhek.khan@gmail.com", :subject => "Tutor accept the stutent's meeting request") do |format|
-        format.text
-        format.html { render "email_sent_successfully" }
-      end
+      mailling_list << superadmin.user.email
     end
+
+    mailling_list << @tutor.email
+
+    #    mail(:to => @student.email, :cc => mailling_list  ,:subject => "Tutor accept the stutent's meeting request")
+
   end
   
   def tutor_reject_meeting_request(meeting_id)
@@ -52,117 +47,124 @@ class UserMailer < ActionMailer::Base
     all_subadmins = Subadmin.all
     all_superadmins = Superadmin.all
 
-    #    mail(:to => @student.email, :subject => "Tutor accept your meeting request")
-    mail(:to => "mahhek.khan@gmail.com", :subject => "Tutor reject your meeting request") do |format|
-      format.text
-      format.html { render "email_sent_successfully" }
-    end
+    mailling_list = []
 
     all_subadmins.each do |subadmin|
       if subadmin.user.department == @student.department
-        #      mail(:to => subadmin.user.email, :subject => "Tutor accept the stutent's meeting request")
-        mail(:to => "mahhek.khan@gmail.com", :subject => "Tutor reject the stutent's meeting request") do |format|
-          format.text
-          format.html { render "email_sent_successfully" }
-        end
+        mailling_list << subadmin.user.email
       end
     end
+
     all_superadmins.each do |superadmin|
-      #      mail(:to => superadmin.user.email, :subject => "Tutor accept the stutent's meeting request")
-      mail(:to => "mahhek.khan@gmail.com", :subject => "Tutor reject the stutent's meeting request") do |format|
-        format.text
-        format.html { render "email_sent_successfully" }
-      end
+      mailling_list << superadmin.user.email
     end
+
+    mailling_list << @tutor.email
+
+    #    mail(:to => @student.email, :cc => mailling_list , :subject => "Tutor reject the stutent's meeting request")
+
   end
 
   def upcoming_meeting_email_twelve_hours_before(meeting_id)
-    #    @meeting = Meeting.find_by_id(meeting_id)
-    #    @tutor = @meeting.tutor.user
-    #    @student = @meeting.user
-    
-    #    all_superadmins = Superadmin.all
-    #    all_superadmins.each do |superadmin|
-    #      # mail(:to => superadmin.user.email, :subject => "Upcoming meeting is still unpaid will only 12 hours remains in the start of meeting time")
-    #      mail(:to => "mahhek.khan@gmail.com", :subject => "Upcoming meeting is still unpaid will only 12 hours remains in the start of meeting time")
-    #    end
+    @meeting = Meeting.find_by_id(meeting_id)
+    @tutor = @meeting.tutor.user
+    @student = @meeting.user
+
+    mailling_list = []
+
+    all_superadmins = Superadmin.all
+    all_superadmins.each do |superadmin|
+      mailling_list << superadmin
+    end
         
-    #    mail(:to => "mahhek.khan@gmail.com", :subject => "Upcoming meeting is still unpaid will only 12 hours remains in the start of meeting time")
-    #    mail(:to => @tutor.email, :subject => "Upcoming meeting is still unpaid will only 12 hours remains in the start of meeting time")
-    #    mail(:to => @student.email, :subject => "Upcoming meeting is still unpaid will only 12 hours remains in the start of meeting time")
+    mailling_list << @tutor.email
+
+    #    mail(:to => @student.email, :cc => mailling_list, :subject => "Upcoming meeting is still unpaid while only 12 hours remains in the start of meeting time")
+
   end
 
   def upcoming_meeting_email_six_hours_before(meeting_id)
-    #    @meeting = Meeting.find_by_id(meeting_id)
-    #    @tutor = @meeting.tutor.user
-    #    @student = @meeting.user
-    #
-    #    all_superadmins = Superadmin.all
-    #    all_superadmins.each do |superadmin|
-    #      # mail(:to => superadmin.user.email, :subject => "Upcoming meeting is still unpaid will only 6 hours remains in the start of meeting time")
-    #      mail(:to => "mahhek.khan@gmail.com", :subject => "Upcoming meeting is still unpaid will only 6 hours remains in the start of meeting time")
-    #    end
+    @meeting = Meeting.find_by_id(meeting_id)
+    @tutor = @meeting.tutor.user
+    @student = @meeting.user
 
-    #    mail(:to => "mahhek.khan@gmail.com", :subject => "Upcoming meeting is still unpaid will only 6 hours remains in the start of meeting time")
-    #    mail(:to => @tutor.email, :subject => "Upcoming meeting is still unpaid will only 6 hours remains in the start of meeting time")
-    #    mail(:to => @student.email, :subject => "Upcoming meeting is still unpaid will only 6 hours remains in the start of meeting time")
+    mailling_list = []
+
+    all_superadmins = Superadmin.all
+    all_superadmins.each do |superadmin|
+      mailling_list << superadmin
+    end
+
+    mailling_list << @tutor.email
+
+    #    mail(:to => @student.email, :cc => mailling_list, :subject => "Upcoming meeting is still unpaid while only 6 hours remains in the start of meeting time")
+
   end
 
   def upcoming_meeting_email_two_hours_before(meeting_id)
-    #    @meeting = Meeting.find_by_id(meeting_id)
-    #    @tutor = @meeting.tutor.user
-    #    @student = @meeting.user
-    #
-    #    all_superadmins = Superadmin.all
-    #    all_superadmins.each do |superadmin|
-    #      # mail(:to => superadmin.user.email, :subject => "Upcoming meeting is still unpaid will only 2 hours remains in the start of meeting time")
-    #      mail(:to => "mahhek.khan@gmail.com", :subject => "Upcoming meeting is still unpaid will only 2 hours remains in the start of meeting time")
-    #    end
+    @meeting = Meeting.find_by_id(meeting_id)
+    @tutor = @meeting.tutor.user
+    @student = @meeting.user
 
-    #    mail(:to => "mahhek.khan@gmail.com", :subject => "Upcoming meeting is still unpaid will only 2 hours remains in the start of meeting time")
-    #    mail(:to => @tutor.email, :subject => "Upcoming meeting is still unpaid will only 2 hours remains in the start of meeting time")
-    #    mail(:to => @student.email, :subject => "Upcoming meeting is still unpaid will only 2 hours remains in the start of meeting time")
+    mailling_list = []
+
+    all_superadmins = Superadmin.all
+    all_superadmins.each do |superadmin|
+      mailling_list << superadmin
+    end
+
+    mailling_list << @tutor.email
+
+    #    mail(:to => @student.email, :cc => mailling_list, :subject => "Upcoming meeting is still unpaid while only 2 hours remains in the start of meeting time")
+
   end
 
   def email_reminder_for_meeting_start(meeting_id)
-    #    @meeting = Meeting.find_by_id(meeting_id)
-    #    @tutor = @meeting.tutor.user
-    #    @student = @meeting.user
-    #    all_subadmins = Subadmin.all
-    #    all_superadmins = Superadmin.all
-    #
-    #    #    mail(:to => @student.email, :subject => "The meeting has started")
-    #    mail(:to => "mahhek.khan@gmail.com", :subject => "The meeting has started")
-    #    all_subadmins.each do |subadmin|
-    #      if subadmin.user.department == @student.department
-    #        #      mail(:to => subadmin.user.email, :subject => "The meeting has started")
-    #        mail(:to => "mahhek.khan@gmail.com", :subject => "The meeting has started")
-    #      end
-    #    end
-    #    all_superadmins.each do |superadmin|
-    #      #      mail(:to => superadmin.user.email, :subject => "The meeting has started")
-    #      mail(:to => "mahhek.khan@gmail.com", :subject => "The meeting has started")
-    #    end
+    @meeting = Meeting.find_by_id(meeting_id)
+    @tutor = @meeting.tutor.user
+    @student = @meeting.user
+    all_subadmins = Subadmin.all
+    all_superadmins = Superadmin.all
+
+    mailling_list = []
+
+    all_subadmins.each do |subadmin|
+      if subadmin.user.department == @student.department
+        mailing_list << subadmin.user.email
+      end
+    end
+
+    all_superadmins.each do |superadmin|
+      mailing_list << superadmin.user.email
+    end
+
+    mailling_list << @tutor.email
+
+    #    mail(:to => @student.email , :cc => mailling_list, :subject => "The meeting has started")
+
   end
   
   def email_reminder_for_meeting_end(meeting_id)
-    #    @meeting = Meeting.find_by_id(meeting_id)
-    #    @tutor = @meeting.tutor.user
-    #    @student = @meeting.user
-    #    all_subadmins = Subadmin.all
-    #    all_superadmins = Superadmin.all
-    #
-    #    #    mail(:to => @student.email, :subject => "The meeting has ended")
-    #    mail(:to => "mahhek.khan@gmail.com", :subject => "The meeting has ended")
-    #    all_subadmins.each do |subadmin|
-    #      if subadmin.user.department == @student.department
-    #        #      mail(:to => subadmin.user.email, :subject => "The meeting has ended")
-    #        mail(:to => "mahhek.khan@gmail.com", :subject => "The meeting has ended")
-    #      end
-    #    end
-    #    all_superadmins.each do |superadmin|
-    #      #      mail(:to => superadmin.user.email, :subject => "The meeting has ended")
-    #      mail(:to => "mahhek.khan@gmail.com", :subject => "The meeting has ended")
-    #    end
+    @meeting = Meeting.find_by_id(meeting_id)
+    @tutor = @meeting.tutor.user
+    @student = @meeting.user
+    all_subadmins = Subadmin.all
+    all_superadmins = Superadmin.all
+
+    mailling_list = []
+
+    all_subadmins.each do |subadmin|
+      if subadmin.user.department == @student.department
+        mailing_list << subadmin.user.email
+      end
+    end
+    all_superadmins.each do |superadmin|
+      mailing_list << superadmin.user.email
+    end
+
+    mailling_list << @tutor.email
+
+    #    mail(:to => @student.email , :cc => mailling_list, :subject => "The meeting has ended")
+
   end
 end
