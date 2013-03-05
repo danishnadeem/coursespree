8b10e3c4e968ed30d754f5c77361628893d59943
mahheki <mahhek.khan@gmail.com>
2013-03-05 18:49:00 +0500

    updated

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,7 +63,7 @@ Findtutor::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
-  config.action_mailer.default_url_options = { :host => 'http://boiling-woodland-4825.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'http://www.coursespree.com' }
   config.action_mailer.delivery_method = :smtp
 
   ActionMailer::Base.smtp_settings = {
