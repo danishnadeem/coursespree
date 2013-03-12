b2ce86cdc8bce48d6466a5390dc324cf274298ff
mahheki <mahhek.khan@gmail.com>
2013-03-12 15:47:25 +0500

    updated

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,6 +2,7 @@ Findtutor::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
   config.action_mailer.default_url_options = { :host => 'http://www.tutorsprout.com' }
+  
 
   # Code is not reloaded between requests
   config.cache_classes = true
