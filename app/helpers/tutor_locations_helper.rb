322ce6e15da505e766a1cb376d16038a09c48b95
mahheki <mahhek.khan@gmail.com>
2013-03-13 13:27:50 +0500

    updated

--- app/helpers/tutor_locations_helper.rb
+++ app/helpers/tutor_locations_helper.rb
@@ -1,6 +1,6 @@
 module TutorLocationsHelper
   def univs_and_ids
-    University.all.map{|u| [u.name, u.id]}
+    University.all.sort_by { |m| m[:name] }.map{|u| [u.name, u.id]}
   end
   def depts_and_ids
     Department.all.map{|d| [d.name, d.id]}
